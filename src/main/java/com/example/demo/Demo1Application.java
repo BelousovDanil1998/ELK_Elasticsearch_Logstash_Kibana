4cec77490a2be0b69ae652865a9606d8f7d97285
Belousov Danil <aleksandrovicdanil93@gmail.com>
2023-05-09 02:11:14 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class Demo1Application {
+    private static final Logger logger = LoggerFactory.getLogger(Demo1Application.class);
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+        logger.info("Hello, world!");
+    }
+
+}
